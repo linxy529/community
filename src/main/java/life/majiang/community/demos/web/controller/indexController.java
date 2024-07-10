62703856a79cfbe87dd558d8420bcf10338f6d7c
lin <linxy040529@aliyun.com>
2024-07-10 14:11:54 +0800

    add index and bootstrap

--- src/main/java/life/majiang/community/demos/web/controller/indexController.java
+++ src/main/java/life/majiang/community/demos/web/controller/indexController.java
@@ -4,14 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+/*
+* create by Xiangyu Lin
+*
+* */
 @Controller //允许controller类接收前端请求
-public class HelloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name="name") String name, Model model )
+public class indexController {
+    @GetMapping("/")
+    public String index()
     {
-        model.addAttribute("name",name);
-        return "hello";
+        return "index";
     }
 
 }
